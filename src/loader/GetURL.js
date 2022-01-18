0eaa0711526eda6abaa6593db8333e16c645523c
guanxiaomin <327547931@qq.com>
2022-01-18 17:24:00 +0800

    update audiofile && gameconfig

--- src/loader/GetURL.js
+++ src/loader/GetURL.js
@@ -15,21 +15,22 @@
  *
  * @return {string} The URL the File will use.
  */
-var GetURL = function (file, baseURL)
-{
-    if (!file.url)
-    {
-        return false;
-    }
-
-    if (file.url.match(/^(?:blob:|data:|capacitor:\/\/|http:\/\/|https:\/\/|\/\/)/))
-    {
-        return file.url;
-    }
-    else
-    {
-        return baseURL + file.url;
-    }
-};
-
-module.exports = GetURL;
+ var GetURL = function (file, baseURL)
+ {
+     if (!file.url)
+     {
+         return false;
+     }
+ 
+     if (file.url.match(/^(?:blob:|data:|capacitor:\/\/|http:\/\/|https:\/\/|\/\/)/))
+     {
+         return file.url;
+     }
+     else
+     {
+         return baseURL + file.url;
+     }
+ };
+ 
+ module.exports = GetURL;
+ 
